caad7ff3658a476fcb08c1d97a0d04c872a7e2ad
Owes Ansari <Owes.Ansari@teknopoint.local>
2023-12-31 16:46:05 +0530

    whats app icon add

---
+++ src/components/WhatsAppButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const WhatsAppButton = () => {
+
+    const phoneNumber = '8655102214';
+    const message = 'hii';
+
+    const handleClick = () => {
+        const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.location.href = whatsappURL;
+    }
+
+  return (
+    <div className='whatsApp'>
+        <button onClick={handleClick}>
+            <img src="whatsapp-logo-1.png" alt="whatsApp-icon" />
+        </button>
+    </div>
+  )
+}
+
+export default WhatsAppButton
